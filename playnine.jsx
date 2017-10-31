8d29ad6d9dd890ec896f7bd252b7cc94ffd60396
nvh95 <nguyenviethunghl00@gmail.com>
2017-10-31 14:34:13 +0700

    Added numbers selection features

--- playnine.jsx
+++ playnine.jsx
@@ -1,13 +1,12 @@
 const Stars = (props) => {
-	const numberOfStars = 1+ Math.floor(Math.random()*9);
   let stars = [];
-  for (let i=0; i< numberOfStars; i++) {
+  for (let i=0; i< props.numberOfStars; i++) {
   	stars.push(<i key={i} className="fa fa-star"></i>);
   }
   
 	return (
   <div className="col-5">
-    {_.range(numberOfStars).map(i=>
+    {_.range(props.numberOfStars).map(i=>
     <i key={i} className="fa fa-star"></i>
     )}
   </div>
@@ -15,9 +14,23 @@ const Stars = (props) => {
 }
 
 const Button = (props) => {
+	let button;
+  switch(props.answerIsCorrect) {
+  	case true:
+    	button = <button className="btn btn-success"><i className="fa fa-check"></i></button>
+      break;
+    case false:
+    	button = <button className="btn btn-danger"><i className="fa fa-times"></i></button>
+      break;
+    default:
+   		button = <button className="btn"
+      onClick={props.checkAnswer}
+      disabled={props.selectedNumbers.length === 0}>=</button>
+      break;
+  }
 	return (
   <div className='col-2'>
-    <button>=</button>
+    {button}
   </div>
   );
 };
@@ -25,18 +38,27 @@ const Button = (props) => {
 const Answer = (props) => {
 	return (
   <div className="col-5">
-    <span>5</span>
-    <span>6</span>
+    {props.selectedNumbers.map((number,i) =>
+    	<span key={i} onClick={() => props.unselectNumber(number)}>{number}</span>
+    )}
   </div>
   );
 };
 
 const Numbers = (props) => {  
+	const numberClassName = (number) => {
+  	if(props.selectedNumbers.indexOf(number) >= 0) {
+    	return "selected";
+    }
+  }
 	return (
   <div className="card text-center">
   	<div>
       {Numbers.list.map((number, i) =>
-      <span key={i}>{number}</span>
+      <span key={i} className={numberClassName(number)}
+      onClick={() => props.selectNumber(number)}>
+      	{number}
+      </span>
       )}
     </div>
   </div>
@@ -46,18 +68,49 @@ const Numbers = (props) => {
 Numbers.list = _.range(1, 10);
 
 class Game extends React.Component {
+	state = {
+  	selectedNumbers: [],
+    randomNumberOfStars: 1+ Math.floor(Math.random()*9),
+    answerIsCorrect: null,
+  };
+  selectNumber = (clickedNumber) => {
+  	if (this.state.selectedNumbers.indexOf(clickedNumber) >= 0) {return;}
+  	this.setState(prevState => ({
+    	answerIsCorrect: null,
+    	selectedNumbers: prevState.selectedNumbers.concat(clickedNumber)
+    }));
+  };
+  unselectNumber = (clickedNumber) => {
+  	this.setState(prevState => ({
+    	answerIsCorrect: null,
+
+    	selectedNumbers: prevState.selectedNumbers.filter(number => number !== clickedNumber)
+    }));
+  };
+  checkAnswer = () => {
+  	this.setState(prevState => ({
+    	answerIsCorrect: prevState.randomNumberOfStars ===
+      	prevState.selectedNumbers.reduce((acc,n) => acc+n,0)
+    }));
+  }
+  
 	render() {
+  	const {selectedNumbers, randomNumberOfStars, answerIsCorrect} = this.state
   	return (
     	<div className="container">
     	  <h3>Play Nine</h3>
         <hr />
         <div className="row">
-          <Stars />
-          <Button />
-          <Answer />
+          <Stars numberOfStars={randomNumberOfStars}/>
+          <Button selectedNumbers={selectedNumbers} 
+          	checkAnswer={this.checkAnswer}
+            answerIsCorrect={answerIsCorrect}/>
+          <Answer selectedNumbers = {selectedNumbers} 
+          unselectNumber={this.unselectNumber}/>
         </div>
         <br />
-        <Numbers />
+        <Numbers selectedNumbers = {selectedNumbers}
+        selectNumber={this.selectNumber} />
     	</div> 
     );
   }
